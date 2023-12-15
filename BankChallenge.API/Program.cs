702beff6d52724028a6dec9f3e7f012015f9934e
Matheus de Assis <assis4002@gmail.com>
2023-12-15 03:14:27 -0300

    fix: enable swagger in prd

--- BankChallenge.API/Program.cs
+++ BankChallenge.API/Program.cs
@@ -20,12 +20,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
